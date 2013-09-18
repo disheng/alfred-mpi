c05969f4c1bff23f34b95d6b405f170229b835d0
Faber89 <fabri.rebecca@gmail.com>
2013-09-19 00:51:13 +0200

    Modified XPathHandler class

--- src/main/java/it/uniroma3/dia/alfredmpi/xpath/XPathHandler.java
+++ src/main/java/it/uniroma3/dia/alfredmpi/xpath/XPathHandler.java
@@ -1,117 +1,57 @@
 package it.uniroma3.dia.alfredmpi.xpath;
 
-import java.io.ByteArrayInputStream;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
+import rules.xpath.XPathRule;
+import model.ExtractedValue;
+import model.Page;
 
 public class XPathHandler {
 	
 	private static XPathHandler instance;	
-	private static DocumentBuilderFactory documentFactory;
-	private static XPathFactory xPathfactory;
 	
 	public static XPathHandler getInstance(){
 		if (instance==null){
-			instance = new XPathHandler();
-			inizializeDocumentFactory();
-			inizializeXPathFactory();		
+			instance = new XPathHandler();		
 		}
 		return instance;		
-	}
-	
-	
-	/**
-	 * 
-	 * Metodo per creare una rappresentazione di un documento da interrogare
-	 * con una query XPath
-	 * 
-	 * @param html - Una stringa che identifica una pagina html
-	 * @return Un oggetto documento che rappresenta la pagina
-	 */
-	public Document buildDocumentFromString(String html){
-		Document doc = null;
-						
-		try {			
-	        DocumentBuilder builder = documentFactory.newDocumentBuilder();		
-	        doc = builder.parse(new ByteArrayInputStream(html.getBytes()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return doc;       
-	}
+	}	
 	
 	/**
 	 * 
-	 * Metodo che esegue una query su un documento
+	 * Metodo che esegue una query su una pagina
 	 * 
-	 * @param doc - Rappresentazione di un documento
+	 * @param page - Rappresentazione di una pagina
 	 * @param xPathQuery - Una Query XPath
-	 * @return NodeList del risultato
+	 * @return ExtractedValue - oggetto risultato della query
 	 */
-	public NodeList executeQuery(Document doc, String xPathQuery){
-				
-		XPath xpath = xPathfactory.newXPath();
-        XPathExpression expr;
-        NodeList nodes = null;
-        
-		try {
-			expr = xpath.compile(xPathQuery);
-			Object result = expr.evaluate(doc, XPathConstants.NODESET);
-			nodes = (NodeList) result;
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();        
-		}        
-        
-        return nodes;
+	public ExtractedValue executeQuery(Page page, String xPathQuery){		
+		XPathRule rule = new XPathRule(xPathQuery);
+		return rule.applyOn(page);		
 	}
 	
 	/**
 	 * 
-	 * Metodo che esegue una query su un documento stampando il risultato su output
+	 * Metodo che esegue una query su una pagina stampando il risultato su output
 	 * 
-	 * @param doc - Rappresentazione di un documento
+	 * @param page - Rappresentazione di una pagina
 	 * @param xPathQuery - Una Query XPath
 	 */
-	public void print_executeQuery(Document doc, String xPathQuery){
-		
-		XPath xpath = xPathfactory.newXPath();
-        XPathExpression expr;
-        NodeList nodes = null;
-        
-		try {
-			expr = xpath.compile(xPathQuery);
-			Object result = expr.evaluate(doc, XPathConstants.NODESET);
-			nodes = (NodeList) result;
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();        
-		}  
-		
-		if(nodes != null){
-	        for(int i = 0; i < nodes.getLength(); ++i) {
-		        System.out.println(nodes.item(i).toString());
-	        }
-		}
-		
-	}
+	public void print_executeQuery(Page page, String xPathQuery){		
+		XPathRule rule = new XPathRule(xPathQuery);
+		ExtractedValue value = rule.applyOn(page);	
+		System.out.println(value.toString());	
+		System.out.println("Number of nodes values : "+value.getNumberOfNodesValues());
+	}	
 	
-	private static void inizializeDocumentFactory(){		
-		if(documentFactory == null)
-			documentFactory = DocumentBuilderFactory.newInstance();		
-	}
-	
-	private static void inizializeXPathFactory() {
-		if (xPathfactory == null){
-			xPathfactory = XPathFactory.newInstance();
-		}		
+	/**
+	 * 
+	 * Costruisce un oggetto Page, rappresentazione di una pagina,
+	 * a partire dalla sua rappresentazione in stringa.
+	 * 
+	 * @param content - contenuto della pagina
+	 * @return Page - oggetto che rappresenta la pagina 
+	 */
+	public Page createPage(String content){
+		Page page = new Page(content);				
+		return page;
 	}
 }
---
+++ src/test/java/it/uniroma3/dia/alfredmpi/xpath/XPathHandler_Test.java
@@ -0,0 +1,36 @@
+package it.uniroma3.dia.alfredmpi.xpath;
+
+import static org.junit.Assert.*;
+
+import model.Page;
+import org.junit.Before;
+import org.junit.Test;
+
+public class XPathHandler_Test {
+	
+	private XPathHandler xpathhandler;
+
+	@Before
+	public void setUp() throws Exception {	
+		xpathhandler = XPathHandler.getInstance();
+	}
+
+	@Test
+	public void test_create_page_from_string() {
+		String content = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";
+		Page page = this.xpathhandler.createPage(content);		
+		assertEquals(content, page.getContent());
+		page.setTitle("Titolo di prova");
+		assertEquals("Titolo di prova", page.getTitle());
+	}
+	
+	@Test
+	public void test_apply_xpath_to_page() {
+		String content = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";		
+		Page page = this.xpathhandler.createPage(content);	
+		page.setTitle("Titolo di prova");
+		XPathHandler.getInstance().print_executeQuery(page, "//a");
+		//TOCOMPLETE
+	}
+
+}
