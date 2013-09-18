d5e6c935425881ba6004b4eb1472e0bce0198031
Faber89 <fabri.rebecca@gmail.com>
2013-09-18 08:22:33 +0200

    Aggiunta classe per la gestione di query XPath su documenti.

--- src/main/java/it/uniroma3/dia/alfredmpi/examples/XPathExample.java
+++ src/main/java/it/uniroma3/dia/alfredmpi/examples/XPathExample.java
@@ -1,5 +1,7 @@
 package it.uniroma3.dia.alfredmpi.examples;
 
+import it.uniroma3.dia.alfredmpi.XPathHandler;
+
 import java.io.ByteArrayInputStream;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -9,10 +11,11 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
+import net.sf.saxon.lib.NamespaceConstant;
+
+import org.apache.hadoop.mapred.gethistory_jsp;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-
-import net.sf.saxon.lib.NamespaceConstant;
 
 public class XPathExample {
 
@@ -24,17 +27,19 @@ public class XPathExample {
 	    // System.setProperty(
 	    // "javax.xml.xpath.XPathFactory", 
 	    // "net.sf.saxon.xpath.XPathFactoryImpl");
-
-	    String xml="<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";
-	    try{
-	    	
+		
+		String xml="<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";
+		String xPathQuery = "//a[matches(.,'#...#')]";
+		
+		/* ESEMPIO SENZA HANDLER
+	    try{	    	
 	        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	        DocumentBuilder builder = factory.newDocumentBuilder();
 	        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
 	        
 	        XPathFactory xPathfactory = XPathFactory.newInstance(NamespaceConstant.OBJECT_MODEL_SAXON);
 	        XPath xpath = xPathfactory.newXPath();
-	        XPathExpression expr = xpath.compile("//a[matches(.,'#...#')]");
+	        XPathExpression expr = xpath.compile(xPathQuery);
 
 	        Object result = expr.evaluate(doc, XPathConstants.NODESET);
 	        NodeList nodes = (NodeList) result;
@@ -46,6 +51,13 @@ public class XPathExample {
 	    catch(Exception e){
 	        e.printStackTrace();
 	    }
+	    */	    
+	    
+	    /* ESEMPIO CON L'HANDLER COSTRUITO */
+	    XPathHandler handler = XPathHandler.getInstance();
+		
+		Document doc = handler.buildDocumentFromString(xml);
+		handler.print_executeQuery(doc, xPathQuery);
 
 	}
 
