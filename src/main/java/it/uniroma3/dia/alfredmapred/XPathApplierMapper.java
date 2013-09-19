247e15d1ab57a1d91e113972a2512a3c7bf81cc6
Faber89 <fabri.rebecca@gmail.com>
2013-09-19 17:18:34 +0200

    Initialized MapRed classes (driver + mapper).

---
+++ src/main/java/it/uniroma3/dia/alfredmapred/XPathApplierMapper.java
@@ -0,0 +1,45 @@
+package it.uniroma3.dia.alfredmapred;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.log4j.Logger;
+
+public class XPathApplierMapper 
+extends Mapper<Text, Text, Text, Text>{ //TODO
+
+	static final Logger log = Logger.getLogger(XPathApplierMapper.class);
+	public enum Counters { DOCS, ERROR, VALID };
+
+	@Override
+	protected void setup(Context context)
+			throws IOException, InterruptedException {
+		//TODO
+	}
+
+	/**
+	 * Maps the input.
+	 *
+	 * @param key The row key.
+	 * @throws java.io.IOException When mapping the input fails.
+	 */
+	@Override
+	public void map(Text key, Text value, Context context) 
+			throws IOException {
+		//TODO
+
+		context.getCounter(Counters.DOCS).increment(1);	
+
+		//APPLY ROLE/ROLES THEN
+		context.getCounter(Counters.VALID).increment(1);	
+
+	try {
+			//WRITE THE OUTPUT
+		} catch (Exception e) {
+			log.error(e.toString());
+			context.getCounter(Counters.ERROR).increment(1);
+		}
+	}		
+
+}
