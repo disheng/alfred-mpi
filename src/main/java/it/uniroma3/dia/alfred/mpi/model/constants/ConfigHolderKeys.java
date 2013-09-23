8b133f66e458c306483a5e7fea5b73b80597c443
H4ml3t <meniluca@gmail.com>
2013-09-23 16:57:35 +0200

    Keys constant for models class

---
+++ src/main/java/it/uniroma3/dia/alfred/mpi/model/constants/ConfigHolderKeys.java
@@ -0,0 +1,7 @@
+package it.uniroma3.dia.alfred.mpi.model.constants;
+
+public class ConfigHolderKeys {
+	
+	public final static String DOMAINS_KEY = "domains";
+
+}
---
+++ src/main/java/it/uniroma3/dia/alfred/mpi/model/constants/DomainHolderKeys.java
@@ -0,0 +1,9 @@
+package it.uniroma3.dia.alfred.mpi.model.constants;
+
+public class DomainHolderKeys {
+	
+	public final static String DOMAIN_ID_KEY = "domain_name";
+	public final static String FIRST_PAGE_KEY = "first_page";
+	public final static String BUCKET_S3_KEY = "bucket_S3";
+
+}
