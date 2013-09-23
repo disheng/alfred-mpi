5a5385e14807c78526d42665875b910942679898
H4ml3t <meniluca@gmail.com>
2013-09-23 17:02:59 +0200

    DomainHolderKeys.java update

--- src/main/java/it/uniroma3/dia/alfred/mpi/model/constants/DomainHolderKeys.java
+++ src/main/java/it/uniroma3/dia/alfred/mpi/model/constants/DomainHolderKeys.java
@@ -5,5 +5,6 @@ public class DomainHolderKeys {
 	public final static String DOMAIN_ID_KEY = "domain_name";
 	public final static String FIRST_PAGE_KEY = "first_page";
 	public final static String BUCKET_S3_KEY = "bucket_S3";
+	public final static String SITE_KEY = "site";
 
 }
