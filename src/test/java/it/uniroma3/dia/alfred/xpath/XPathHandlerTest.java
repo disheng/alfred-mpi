761133feacc52a368076a609097d84b89ea6ce5f
Francesco Furiani <francesco@furio.me>
2013-09-28 13:28:15 +0200

    Updated gitignore Fixed test classes

--- src/test/java/it/uniroma3/dia/alfred/xpath/XPathHandlerTest.java
+++ src/test/java/it/uniroma3/dia/alfred/xpath/XPathHandlerTest.java
@@ -6,23 +6,23 @@ import model.Page;
 import org.junit.Test;
 
 public class XPathHandlerTest {
-
+	public static final String PAGE_TITLE = "Titolo di prova";
+	
 	@Test
 	public void test_create_page_from_string() {
 		String content = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";
 		Page page = new Page(content);		
 		assertEquals(content, page.getContent());
-		page.setTitle("Titolo di prova");
-		assertEquals("Titolo di prova", page.getTitle());
+		page.setTitle(PAGE_TITLE);
+		assertEquals(PAGE_TITLE, page.getTitle());
 	}
 	
 	@Test
 	public void test_apply_xpath_to_page() {
 		String content = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";		
 		Page page = new Page(content);	
-		page.setTitle("Titolo di prova");
-		XPathHandler.print_executeQuery(page, "//a");
-		//TOCOMPLETE
+		page.setTitle(PAGE_TITLE);
+		assertEquals(6, XPathHandler.executeQuery(page, "//*").getNumberOfNodesValues());
 	}
 
 }
