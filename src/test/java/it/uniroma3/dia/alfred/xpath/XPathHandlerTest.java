66b1e6f26f00123e7d6acf8c147f01aaafcfabec
Francesco Furiani <francesco@furio.me>
2013-09-29 12:48:00 +0200

    Explicit compile phase in jar generation Added maven-compile-plugin version (removing warning) Moved document content to final string in XPathHandlerTest

--- src/test/java/it/uniroma3/dia/alfred/xpath/XPathHandlerTest.java
+++ src/test/java/it/uniroma3/dia/alfred/xpath/XPathHandlerTest.java
@@ -7,20 +7,19 @@ import org.junit.Test;
 
 public class XPathHandlerTest {
 	public static final String PAGE_TITLE = "Titolo di prova";
+	public static final String PAGE_CONTENT = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";
 	
 	@Test
 	public void test_create_page_from_string() {
-		String content = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";
-		Page page = new Page(content);		
-		assertEquals(content, page.getContent());
+		Page page = new Page(PAGE_CONTENT);		
+		assertEquals(PAGE_CONTENT, page.getContent());
 		page.setTitle(PAGE_TITLE);
 		assertEquals(PAGE_TITLE, page.getTitle());
 	}
 	
 	@Test
-	public void test_apply_xpath_to_page() {
-		String content = "<root><a>#BBB#</a><a>#CCC#</a><b><a>#DDD#</a></b></root>";		
-		Page page = new Page(content);	
+	public void test_apply_xpath_to_page() {	
+		Page page = new Page(PAGE_CONTENT);	
 		page.setTitle(PAGE_TITLE);
 		assertEquals(6, XPathHandler.executeQuery(page, "//*").getNumberOfNodesValues());
 	}
