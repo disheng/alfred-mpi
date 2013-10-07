487c739f166cae99ca82d54d52490ddaa8fb0f5a
Francesco Furiani <francesco@furio.me>
2013-10-07 21:26:13 +0200

    Start/stop MPJ script

---
+++ runningScripts/mpjCluster.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MPJ_BOOT=`which mpjboot`
+MPJ_HALT=`which mpjhalt`
+MACHINEFILENAME=machinelist
+SCRIPT_NAME=`basename $0`
+
+if [ -z "$MPJ_BOOT" ] || [ -z "$MPJ_HALT" ]
+then
+	echo "mpjboot or mpjhalt executable not found."
+	exit 1
+fi
+
+if ! [ -f $MACHINEFILENAME ]
+then
+        echo "$MACHINEFILENAME not found."
+        exit 1
+fi
+
+parse_script_option_list () {
+	case "$1" in
+		start)
+			echo "Starting MPJ cluster"
+			$MPJ_BOOT $MACHINEFILENAME
+		;;
+		stop)
+			echo "Stopping MPJ cluster"
+			$MPJ_HALT $MACHINEFILENAME
+		;;
+		*)
+            cat << EOF >&2
+Usage: $SCRIPT_NAME {start|stop|restart|force-reload|status}
+EOF
+            exit 1
+            ;;
+    esac
+}
+
+parse_script_option_list $@
