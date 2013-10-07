61a30ff0c756309425659e6364646dc9a3db4452
Francesco Furiani <francesco@furio.me>
2013-10-07 12:37:05 +0200

    configurator scrpts

--- runAlfredMPI.sh
+++ runAlfredMPI.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-OMPI_PATH=/home/furio/ompi-1.7
-OMPI_BIN=$OMPI_PATH/bin
+HOMEDIR=/home/`whoami`
+MACHINEFILENAME=machinelist
+MACHINEFILE=$HOMEDIR/$MACHINEFILENAME
+
+MPJ_HOME=$HOMEDIR/mpj-v0_38
+MPJ_BIN=$MPJ_HOME/bin
 OMPI_LIB=$OMPI_PATH/lib
 
 JAR_FILE=alfred-mpi-0.0.1-SNAPSHOT-mpi.jar
@@ -10,7 +14,14 @@ OMPI_SLAVES=2
 OMPI_PROC_COUNT=$(($OMPI_MASTER+$OMPI_SLAVES))
 JAVA_MEMORY_OPTIONS="-XX:MaxPermSize=128m -Xmx2G"
 
-export PATH=$OMPI_BIN:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OMPI_LIB
+export $MPJ_HOME
+export PATH=$PATH:$MPJ_BIN
 
-mpirun -n $OMPI_PROC_COUNT java -d64 $JAVA_MEMORY_OPTIONS -Djava.library.path=$OMPI_LIB -jar $JAR_FILE $@
+if [ -f $MACHINEFILE ]
+then
+	echo "Starting on multiple machines"
+	mpjrun.sh -np $OMPI_PROC_COUNT $JAVA_MEMORY_OPTIONS -dev niodev -machinesfile $MACHINEFILE -jar $JAR_FILE $@
+else
+	echo "Starting on multicore"
+	mpjrun.sh -np $OMPI_PROC_COUNT $JAVA_MEMORY_OPTIONS -jar $JAR_FILE $@
+fi
