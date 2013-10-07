20fb20ef68d8977c55a38c3dda8b64d3fb0d9c79
Francesco Furiani <fra.furiani@stud.uniroma3.it>
2013-10-07 21:05:55 +0200

    Update configClusterJava.sh

--- runningScripts/configClusterJava.sh
+++ runningScripts/configClusterJava.sh
@@ -13,8 +13,7 @@ then
 	echo "Configuring each machine through ssh"
 	cat $MACHINEFILE | while read line; do 
 	    echo "Configuring machine: $line"
-	    ssh $line "apt-get update && apt-get install openjdk-7-jdk -y"
-	    ssh $line "update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java"
+	    ssh $line "apt-get update && apt-get install openjdk-7-jdk -y && update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java"
 	done
 else
 	echo "No machine file found. Configuring only current"
